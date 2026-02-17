45ea1869f7b9d4ef693a75628a84ad217cb67f33
JamesHinson <jamesmasonhinson@gmail.com>
2026-02-16 16:17:54 -0800

    Added install.sh file; needs implementation

---
+++ install.sh
@@ -0,0 +1,13 @@
+# TODO - Write a script to install the following:
+# - Nmap
+# - Dirb
+# - DNSenum
+# - Whois
+# - WhatWeb
+# - Wappalyzer
+# - Nikto
+# - Sublist3r
+# - Xsser
+# - SQLMap
+# - Nuclei
+# - Amass
